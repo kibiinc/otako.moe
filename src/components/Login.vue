5b7db7bb42b861f7119af44a6aadad41869fa16d
Pranoy Dutta <pranoydll@gmail.com>
2017-11-22 13:48:59 -0600

    modified Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>Otakoapp</h1>
+    <div class="login-container">
+      <form class="login-form" action="" method="post">
+        Email:<br><input type="email" name="" value=""><br>
+        Password:<br><input type="password" name="" value="">
+      </form>
+    </div>
   </div>
 </template>
 
@@ -15,4 +21,7 @@ export default {
 </script>
 
 <style scoped>
+.login-container {
+  margin: auto;
+}
 </style>
