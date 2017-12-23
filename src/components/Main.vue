7f566307d50d7ff8dc74cbb98d09cf5510954dbc
Andreas P <apap04@otakoapp.com>
2017-12-23 14:09:34 -0500

    Organized chan-wrapper and main flex.
    This finally fixes how they both behave.
    This took me a while to do this so it's not THAT easy.
    
    Signed-off-by: Andreas P <apap04@otakoapp.com>

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,12 @@
 <template>
   <!-- for now, client'll be unlocked -->
-  <div class="main flex"> 
-    <p>test<p>
-    <div class="chan-wrapper">
-      <div class="scroll">
-        <!-- insert guilds here. -->
+    <div class="main flex">
+      <div class="chan-wrapper">
+        <div class="scroll">
+        </div>
       </div>
+      <p>test</p>
     </div>
-  </div>
 </template>
 
 <script>
@@ -42,4 +41,22 @@ export default {
 p 
   margin: 0
   height: 0
+
+usr-info
+  align-items: center
+  display: flex
+  margin-bottom: 1px
+  padding: 0 10px
+  height: 52px
+  background: rgba(32,34,37,.3)
+  color: #fff
+
+.srv-chans
+  background-color: #8c857e
+  height: 240px
+  width: 444px
+
+.column
+  flex-direction: column
+
 </style>
